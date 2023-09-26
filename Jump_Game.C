1617e80ef477301b1612904200d18ab6dcc6c486
RahulKumar-007 <chhonkarrahul@gmail.com>
2023-09-26 11:47:13 +0530

    Add new files

---
+++ Jump_Game.C
@@ -0,0 +1,32 @@
+/*
+ You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+*/
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int last_pos = n-1;
+        for(int i = n-1 ; i > -1 ; i--)
+        {
+            if(nums[i]+i >= last_pos) last_pos = i;
+        }
+        if(last_pos==0)return true;
+        return false;
+    }
+};
