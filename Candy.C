8e550637eb323e59e9abff48262b49691f393cfb
Rahul <chhonkarrahul1362@gmail.com>
2023-10-16 00:45:31 +0530

    pushing new files

---
+++ Candy.C
@@ -0,0 +1,55 @@
+/*
+ 
+
+   There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+Return the minimum number of candies you need to have to distribute the candies to the children.
+
+
+
+Example 1:
+
+Input: ratings = [1,0,2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+Example 2:
+
+Input: ratings = [1,2,2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+The third child gets 1 candy because it satisfies the above two conditions.
+
+
+*/
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int ans=0;
+        int n=ratings.size();
+        vector<int> candies(n,1);
+        for(int i=1;i<ratings.size();i++)
+        {
+            if(ratings[i-1]<ratings[i])
+            {
+                candies[i]=candies[i-1]+1;
+            }
+        }
+       for(int i=n-2;i>-1;i--)
+        {
+            if(ratings[i+1]<ratings[i])
+            {
+                candies[i]=max(candies[i],candies[i+1]+1);  // important case 
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            ans+=candies[i];
+        }
+        return ans;
+    }
+};
