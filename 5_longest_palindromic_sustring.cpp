b9bb987e92a0955304e918019e7ad0098b2c4c66
Rahul <117337265+RahulKumar-007@users.noreply.github.com>
2024-07-22 12:58:46 +0530

    Add files via upload

---
+++ 5_longest_palindromic_sustring.cpp
@@ -0,0 +1,60 @@
+/*
+
+Given a string s, return the longest
+palindromic
+substring
+in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 1000
+    s consist of only digits and English letters.
+
+
+   */
+
+class Solution {
+public:
+    std::string longestPalindrome(std::string s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        
+        int max_len = 1;
+        int start = 0;
+        int end = 0;
+        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
+        
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > max_len) {
+                        max_len = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
+        }
+        
+        return s.substr(start, end - start + 1);
+    }
+};
+
